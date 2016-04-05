1dc87d66cd45bad1191043161ed59d844f2b43a7
Mark Oliver Jensen <mark_oliver_jensen@hotmail.com>
2016-04-05 11:14:26 +0200

    Table v0.1

--- config.php
+++ config.php
@@ -4,21 +4,38 @@ return [
     'connection' => [
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'password',
+        'password' => 'Password',
         'database' => 'admin',
         'charset'  => 'utf8',
     ],
     'tables'     => [
         'alias' => [
+            'user' => 'Users'
         ],
         'user'  => [
-            'excludes' => [
-                'UserId'
+            'UserId' => [
+                'hidden' => true
             ],
-
+            'UserUsername' => [
+                'type' => 'string',
+                'length' => 64,
+                'alias' => 'Username',
+            ],
+            'UserEmail' => [
+                'type' => 'email',
+                'length' => 127,
+                'alias' => 'Email',
+            ],
             'UserPassword' => [
-                'type' => 'password'
-            ]
+                'type' => 'password',
+                'length' => 64,
+                'alias' => 'Password',
+            ],
+            'UserLevel' => [
+                'type' => 'int',
+                'length' => 4,
+                'alias' => 'Level',
+            ],
 
         ],
     ],
--- web/table.php
+++ web/table.php
@@ -2,3 +2,36 @@
 
 require '../vendor/autoload.php';
 
+if (isset($_GET['table']))
+    $table = $_GET['table'];
+
+else
+    header('location: index.php');
+
+$db     = get_connection();
+$config = get_config();
+
+$info = $config['tables'][ $table ];
+$data = $db->get($table);
+$info = array_filter($info, function ($value) {
+    return !(array_key_exists('hidden', $value) && $value['hidden'] === TRUE);
+});
+?>
+
+<table>
+    <caption><?= $table ?></caption>
+    <thead>
+    <tr>
+        <?php
+        var_dump($info);
+        foreach ($info as $key => $item) { ?>
+            <th><?= $item['alias'] ?? $key ?? null ?></th>
+        <?php } ?>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>data</td>
+    </tr>
+    </tbody>
+</table>
