89ee45ecd0ddbc250de3e47f8154180402e4bf55
Thomas Rosenkrans Vestergaard <523604@RTS-365.dk>
2016-04-05 09:14:17 +0200

    added mysqli wrapper

--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    $file = '../src/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($file)) {
-        include_once $file;
-    }
-});
