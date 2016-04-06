60ac6fe45cff57f7d84aec0fb08f94509760180a
Mark Oliver Jensen <mark_oliver_jensen@hotmail.com>
2016-04-06 13:35:52 +0200

    test

--- web/users.php
+++ web/users.php
@@ -5,11 +5,11 @@ require 'includes/top.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'list';
 
-$crudl = new \CRUDL\CRUDL('localhost', 'root', 'password', 'admin', 'user');
+$crudl = new \CRUDL\CRUDL('127.0.0.1', 'root', 'Password', 'admin', 'user');
 $crudl->globalJoin('image', 'UserImage = ImageId');
 $crudl->list->columns(['UserId', 'UserUsername', 'ImageText']);
 $crudl->list->closure('UserId', function ($value) {
-    return $value - 1;
+    return '<a href="users.php?update=' . $value . '">Hello world</a>';
 });
 
 echo $crudl->action($action);
