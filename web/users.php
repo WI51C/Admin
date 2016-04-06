bdfc3faf5b0f8d10e376644443f0bdb6b12c6395
ThomasKasper <thom-mas@hotmail.com>
2016-04-06 20:38:38 +0200

    table css

--- web/users.php
+++ web/users.php
@@ -1,17 +1,19 @@
 <?php
 
+use CRUDL\CRUDL;
+
 require '../vendor/autoload.php';
 require 'includes/top.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'list';
 
-$crudl = new \CRUDL\CRUDL('127.0.0.1', 'root', 'Password', 'admin', 'user');
-$crudl->globalJoin('image', 'UserImage = ImageId');
-$crudl->list->columns(['UserId', 'UserUsername', 'ImageText']);
+$crudl = new CRUDL('127.0.0.1', 'root', 'password', 'admin', 'user');
+$crudl->list->join('image', 'ImageId = UserImage', 'INNER');
+$crudl->list->columns(['UserId' => 'ID', 'UserUsername', 'UserPassword', 'ImageText']);
+$crudl->list->noescape(['UserId']);
 $crudl->list->closure('UserId', function ($value) {
     return "<a href='users.php?action=update&id=$value'>Update</a>";
 });
-$crudl->list->htmlColumns(['UserId']);
 
 echo $crudl->action($action);
 
