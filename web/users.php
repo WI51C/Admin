d73a9e65e06cace4534a583ac51512f4ae49007e
ThomasKasper <thom-mas@hotmail.com>
2016-04-06 11:12:22 +0200

    update

--- web/users.php
+++ web/users.php
@@ -1,3 +1,10 @@
 <?php
 
 require '../vendor/autoload.php';
+
+$action = isset($_GET['action']) ? $_GET['action'] : 'list';
+
+$crudl = new \CRUDL\CRUDL('localhost', 'root', 'password', 'admin', 'user');
+$crudl->globalJoin('image', 'UserImage = ImageId');
+
+echo $crudl->action($action);
