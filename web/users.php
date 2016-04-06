3c22797a4133aee0251c2d128b42fbe82ab4e3f1
ThomasKasper <thom-mas@hotmail.com>
2016-04-06 13:26:03 +0200

    added includes

--- web/users.php
+++ web/users.php
@@ -1,10 +1,17 @@
 <?php
 
 require '../vendor/autoload.php';
+require 'includes/top.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'list';
 
 $crudl = new \CRUDL\CRUDL('localhost', 'root', 'password', 'admin', 'user');
 $crudl->globalJoin('image', 'UserImage = ImageId');
+$crudl->list->columns(['UserId', 'UserUsername', 'ImageText']);
+$crudl->list->closure('UserId', function ($value) {
+    return $value - 1;
+});
 
 echo $crudl->action($action);
+
+require 'includes/bottom.php';
