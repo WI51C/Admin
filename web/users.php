ff17acf52bde4d3fa8eccbb03c685ab5b02fe139
ThomasKasper <thom-mas@hotmail.com>
2016-04-07 12:38:10 +0200

    yolo

--- web/users.php
+++ web/users.php
@@ -3,16 +3,19 @@
 require '../vendor/autoload.php';
 require 'includes/top.php';
 
-$action = isset($_GET['action']) ? $_GET['action'] : 'list';
-
-$controller = new \CRUDL\CrudController('127.0.0.1', 'root', 'password', 'admin', 'user');
-$controller->table->join('image', 'ImageId = UserImage', 'INNER');
-$controller->table->columns(['UserId' => 'ID', 'UserUsername', 'UserPassword', 'ImageText']);
-$controller->table->noescape(['UserId']);
-$controller->table->closure('UserId', function ($value) {
-    return "<a href='users.php?action=update&id=$value'>Update</a>";
-});
-
-echo $controller->action($action);
+//$action = isset($_GET['action']) ? $_GET['action'] : 'table';
+//
+//$controller = new \CRUDL\CrudController('127.0.0.1', 'root', 'password', 'admin', 'user');
+//$controller->table->map->newOTO(function ($relation) {
+//    $relation->joinTable('image');
+//    $relation->joinCondition('UserImage = ImageId');
+//});
+//$controller->table->columns(['UserId' => 'ID', 'UserUsername', 'UserPassword', 'ImageText']);
+//$controller->table->noescape(['UserId']);
+//$controller->table->closure('UserId', function ($value) {
+//    return "<a href='users.php?action=update&id=$value'>Update</a>";
+//});
+//
+//echo $controller->action($action);
 
 require 'includes/bottom.php';
