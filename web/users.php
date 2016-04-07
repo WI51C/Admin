b355d938b04cb547dae1a84b804e3bb9eb4b9be9
ThomasKasper <thom-mas@hotmail.com>
2016-04-07 09:47:30 +0200

    u

--- web/users.php
+++ web/users.php
@@ -1,20 +1,18 @@
 <?php
 
-use CRUDL\CRUDL;
-
 require '../vendor/autoload.php';
 require 'includes/top.php';
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'list';
 
-$crudl = new CRUDL('127.0.0.1', 'root', 'password', 'admin', 'user');
-$crudl->list->join('image', 'ImageId = UserImage', 'INNER');
-$crudl->list->columns(['UserId' => 'ID', 'UserUsername', 'UserPassword', 'ImageText']);
-$crudl->list->noescape(['UserId']);
-$crudl->list->closure('UserId', function ($value) {
+$controller = new \CRUDL\CrudController('127.0.0.1', 'root', 'password', 'admin', 'user');
+$controller->table->join('image', 'ImageId = UserImage', 'INNER');
+$controller->table->columns(['UserId' => 'ID', 'UserUsername', 'UserPassword', 'ImageText']);
+$controller->table->noescape(['UserId']);
+$controller->table->closure('UserId', function ($value) {
     return "<a href='users.php?action=update&id=$value'>Update</a>";
 });
 
-echo $crudl->action($action);
+echo $controller->action($action);
 
 require 'includes/bottom.php';
