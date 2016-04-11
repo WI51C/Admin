6831d32fb303b58ad709876a051e220264fba074
ThomasKasper <thom-mas@hotmail.com>
2016-04-11 15:02:53 +0200

    inline table max height

--- web/admin.php
+++ web/admin.php
@@ -4,10 +4,10 @@ require '../vendor/autoload.php';
 require 'includes/top.php';
 
 $controller = new Admin\CRUD('localhost', 'root', 'password', 'admin', 'user');
-$controller->read->presentation->columns(['UserUsername' => 'Username', 'UserPassword' => 'Password', 'ImageText' => 'Best']);
+$controller->read->presentation->columns(['UserUsername' => 'Username', 'UserPassword' => 'Password', 'ImageText' => 'Image Text']);
 $controller->read->presentation->caption('Users');
 $controller->read->select->relations->oto('Image', 'ImageId = UserImage');
-$controller->read->modifiers->add('UserUsername', 'strtoupper');
+$controller->read->modifiers->add('UserUsername', 'ucwords');
 $controller->read->select->relations->otm('attributes', 'UserId', 'AttributeUserId', 'INNER', function ($table) {
     $table->alias('Attributes');
     $table->presentation->columns([
