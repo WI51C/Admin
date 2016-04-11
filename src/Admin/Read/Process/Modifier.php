bbcffa7963ed7594319240a6e7afbd9469b79816
ThomasKasper <thom-mas@hotmail.com>
2016-04-11 14:31:00 +0200

    added modifiers

---
+++ src/Admin/Read/Process/Modifier.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Admin\Read\Process;
+
+
+class Modifier
+{
+
+}
---
+++ src/Admin/Read/Tables/Modifiers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Admin\Read\Tables;
+
+class Modifiers
+{
+
+    public function link()
+    {
+
+    }
+
+    public function image()
+    {
+
+    }
+
+    public function date()
+    {
+
+    }
+
+    public function htmlentities()
+    {
+
+    }
+
+    public function lowercase()
+    {
+
+    }
+
+    public function uppercase()
+    {
+
+    }
+
+    public function ucfirst()
+    {
+
+    }
+}
