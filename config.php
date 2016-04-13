d2f6f245ffc0c3eea1de3f3040af9cadcf8e63fb
ThomasKasper <thom-mas@hotmail.com>
2016-04-13 10:14:42 +0200

    start of config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    // Connection information of the database.
+    //
+    // Hostname: the hostname of the database.
+    // Username: the username to the database.
+    // Password: the password to the database.
+    // Database: the database to access.
+    'connection' => [
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => 'password',
+        'database' => 'database',
+    ],
+];
