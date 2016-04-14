49e314db560a2b462c1402c2738276a2724458ad
ThomasKasper <thom-mas@hotmail.com>
2016-04-14 13:37:46 +0200

    open descendant tables in place of the main table

--- web/admin.php
+++ web/admin.php
@@ -6,9 +6,6 @@ require 'includes/top.php';
 $connection = new \Admin\Connection('localhost', 'root', 'password', 'admin');
 $read       = new \Admin\Read\Read($connection);
 $read->setTable('user');
-$read->setCaption('USERS');
-$read->columns->addColumn('user.userid', 'ID', 2);
-$read->columns->addColumn('user.userusername', 'Username', 1);
 $read->relations->newOTO('image', 'image.ImageId = user.UserImage');
 $read->relations->newMTM('roles', 'user.UserId', 'roles_user_relation.UserID', 'roles_user_relation', 'roles.RoleID = roles_user_relation.RoleID', 'INNER')
                 ->setCaption('Roles')
--- web/js/inline-table-inspector.js
+++ web/js/inline-table-inspector.js
@@ -4,9 +4,14 @@ $(function () {
         var container = $(this).siblings('.table-container');
         var table = container.children('table');
         container.addClass('inspect-active');
+        container.width($('#content').width());
     });
 
     $('.table-inspect-exit').click(function () {
         $(this).closest('.table-container').removeClass('inspect-active');
+    });
+
+    $(window).resize(function () {
+        $('.inspect-active').width($('#content').width());
     });
 });
