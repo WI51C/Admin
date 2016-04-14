1ac81fdcd16b1b4017d0adf9ce8c2cbd88da6f19
ThomasKasper <thom-mas@hotmail.com>
2016-04-14 11:27:56 +0200

    connection expansion

--- src/Admin/Connection.php
+++ src/Admin/Connection.php
@@ -12,7 +12,7 @@ class Connection
      *
      * @var MysqliDb
      */
-    public $connection;
+    protected $connection;
 
     /**
      * Hostname of the database connection.
@@ -63,6 +63,18 @@ class Connection
     }
 
     /**
+     * Sets the new connection using the properties of the Connection instance.
+     *
+     * @return $this
+     */
+    public function connect()
+    {
+        $this->connection = new MysqliDb($this->hostname, $this->username, $this->password, $this->database);
+
+        return $this;
+    }
+
+    /**
      * Clones the connection of the Connection instance.
      *
      * @return MysqliDb
@@ -70,5 +82,99 @@ class Connection
     public function query()
     {
         return clone $this->connection;
+    }
+
+    /**
+     * Gets the hostname of the connection.
+     *
+     * @return string
+     */
+    public function getHostname()
+    {
+        return $this->hostname;
+    }
+
+    /**
+     * Sets the hostname of the connection.
+     *
+     * @param string $hostname the hostname to set.
+     *
+     * @return $this
+     */
+    public function setHostname(string $hostname)
+    {
+        $this->hostname = $hostname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Sets the username of the of the connection.
+     *
+     * @param string $username the username to set.
+     *
+     * @return $this
+     */
+    public function setUsername(string $username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Gets the password of the connection.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Sets the password of the connection
+     *
+     * @param string $password the password to set.
+     *
+     * @return $this
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Gets the database name of the connection.
+     *
+     * @return string
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    /**
+     * Sets the database name of the connection.
+     *
+     * @param string $database the name to set.
+     *
+     * @return $this
+     */
+    public function setDatabase(string $database)
+    {
+        $this->database = $database;
+
+        return $this;
     }
 }
