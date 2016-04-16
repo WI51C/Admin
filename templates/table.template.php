c1f65983fa58c772930bc38514cd2b707506ed3b
ThomasKasper <thom-mas@hotmail.com>
2016-04-16 17:14:19 +0200

    added table classes

--- templates/table.template.php
+++ templates/table.template.php
@@ -7,25 +7,25 @@
             <a class="waves-effect waves-light btn-large btn-medium red table-inspect-exit"><?= $this->table->closeMessage ?></a>
         </div>
     <?php endif ?>
-    <table>
+    <table class="table">
         <?php if ($this->table->caption !== null): ?>
-            <caption><?= $this->table->caption ?></caption>
+            <caption class="table-caption"><?= $this->table->caption ?></caption>
         <?php endif ?>
         <?php if ($this->table->head): ?>
-            <thead>
-            <tr>
+            <thead class="table-head">
+            <tr class="table-head-row">
                 <?php foreach ($this->columns as $column): ?>
-                    <th><?= $column->header ?></th>
+                    <th class="table-header"><?= $column->header ?></th>
                 <?php endforeach ?>
             </tr>
             </thead>
         <?php endif ?>
         <?php if (!empty($this->data)): ?>
-            <tbody>
+            <tbody class="table-body">
             <?php foreach ($this->data as $position => $row): ?>
-                <tr>
+                <tr class="table-row">
                     <?php foreach ($this->columns as $column): ?>
-                        <td><?= $column->modifier ? call_user_func($column->modifier, $row[$column->name]) : $row[$column->name] ?></td>
+                        <td class="table-data"><?= $column->modifier ? call_user_func($column->modifier, $row[$column->name]) : $row[$column->name] ?></td>
                     <?php endforeach ?>
                 </tr>
             <?php endforeach ?>
