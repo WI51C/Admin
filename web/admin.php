f832183bf06d0379ac5ef9b1cbdcc9dd082443ee
ThomasKasper <thom-mas@hotmail.com>
2016-04-18 14:59:00 +0200

    u

--- web/admin.php
+++ web/admin.php
@@ -7,7 +7,7 @@ $connection = new \Admin\Connection('localhost', 'root', 'password', 'admin');
 $read       = (new \Admin\Read\Read($connection));
 $read->setTable('user');
 $read->setCaption('Users');
-$read->columns->add('userusername', 'Username', 1)->apply('strtoupper')->setSelectable(true);
+$read->columns->displayAll();
 $read->relations->oto('image', 'image.ImageId = user.UserImage');
 $read->relations->mtm('roles', 'user.UserId', 'roles_user_relation.UserID', 'roles_user_relation', 'roles.RoleID = roles_user_relation.RoleID', 'INNER')->columns->displayAll();
 $read->relations->otm('attributes', 'user.UserId', 'attributes.AttributeUserId')->columns->displayAll();
